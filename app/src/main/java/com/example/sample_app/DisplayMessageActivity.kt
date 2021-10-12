d766be7deac7f55230b4e3ac5e6f4e4026cb15ca
dcKim <kdc7140@naver.com>
2021-10-12 17:08:54 +0900

    add file

---
+++ app/src/main/java/com/example/sample_app/DisplayMessageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sample_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+    }
+}
