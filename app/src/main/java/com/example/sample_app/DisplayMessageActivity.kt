d0958b92af8ca89f345131c94f75f8665b76639d
dcKim <kdc7140@naver.com>
2021-10-13 15:18:15 +0900

    sample project

---
+++ app/src/main/java/com/example/sample_app/DisplayMessageActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sample_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayMessageAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message_acitivity)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
