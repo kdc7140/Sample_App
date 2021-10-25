af01e67799b640d8b579f7a01ffdafe119ecc101
dcKim <kdc7140@naver.com>
2021-10-25 17:48:02 +0900

    add test sample

--- app/src/main/java/com/example/sample_app/MainActivity.kt
+++ app/src/main/java/com/example/sample_app/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.sample_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import java.lang.NumberFormatException
 
 const val EXTRA_MESSAGE = "com.example.sample_app.MESSAGE"
 
@@ -12,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button3)
+        button.setOnClickListener{
+            solution()
+        }
     }
 
     /** Called when the user taps the Send button */
@@ -24,9 +33,35 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    //더하기
-    fun sumNumber(){
-        var num1 = findViewById<EditText>(R.id.sumNum1)
-        var num2 = findViewById<EditText>(R.id.sumNum2)
+    fun msgTest(){
+        Log.d("KDC", "test")
+    }
+
+
+    fun solution() {
+        Log.d("KDC", "start")
+        var editCt = findViewById<EditText>(R.id.editCt)
+        var answer:Int = 0
+        var count = Integer.parseInt(editCt.text.toString()).toLong()
+
+
+        while(count != 1.toLong()){
+            count = if(count%2 == 0.toLong()){
+                count/2
+            }else{
+                count*3+1
+            }
+            answer++
+
+            if(answer == 500){
+                answer = -1
+            }
+        }
+
+        var textView2 = findViewById<TextView>(R.id.textView2).apply{
+            text = answer.toString()
+        }
+
+        //return answer
     }
 }
