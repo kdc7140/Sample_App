88a06c9e1c26096944ba426d362855b9b33313c4
dcKim <kdc7140@naver.com>
2021-10-27 17:54:56 +0900

    calc sample progrssing

--- app/src/main/java/com/example/sample_app/Calculator.kt
+++ app/src/main/java/com/example/sample_app/Calculator.kt
@@ -2,10 +2,21 @@ package com.example.sample_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        var addZero : Button = findViewById(R.id.button22)
+        addZero.setOnClickListener {
+            var thisNum = findViewById<Button>(R.id.button22).text.toString()
+
+            var textView = findViewById<TextView>(R.id.editTextTextPersonName2).apply {
+                text = thisNum
+            }
+        }
     }
 }
