9b50f61f13c0d4d8c09fd606370892d661b137fd
dcKim <kdc7140@naver.com>
2021-11-01 17:45:15 +0900

    calc sample is being developed

--- app/src/main/java/com/example/sample_app/Calculator.kt
+++ app/src/main/java/com/example/sample_app/Calculator.kt
@@ -5,12 +5,16 @@ import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
     }
+
+    var calcNum : Int = 0;
+    var calcFlag : Int = 0;
 
     fun buttonClicked(v: View) {
         when (v.id) {
@@ -38,12 +42,36 @@ class Calculator : AppCompatActivity() {
 
     //숫자버튼
     private fun btnNumClick(num: String){
-        findViewById<TextView>(R.id.resultView).append(num)
+        if(calcFlag == 0){
+            resultView.append(num)
+            Log.d("KDCKDC1", "calcFlag : 0")
+        }else {
+            resultView.setText("")
+            resultView.append(num)
+            calcFlag = 0
+            Log.d("KDCKDC2", "calcFlag : 1")
+        }
     }
 
     //연산기호버튼
     private fun btnOperatorClicked(symbol: String){
 
+        var rv1 = Integer.parseInt(resultView.text.toString())
+        calcFlag = 1
+
+        if(resultView2.text != ""){
+            var rv2Text = resultView2.text.toString()
+            var rv2Num = rv2Text.substring(0, rv2Text.length-1)
+            calcNum = Integer.parseInt(rv2Num)
+            Log.d("KDCKDC3", calcFlag.toString() )
+
+            resultView.setText((calcNum +rv1).toString())
+            resultView2.setText(resultView.text.toString() + symbol)
+            calcNum = 0
+        }else{
+            resultView2.setText(resultView.text.toString() + symbol)
+            resultView.setText("")
+        }
     }
 
     //초기화 버튼
