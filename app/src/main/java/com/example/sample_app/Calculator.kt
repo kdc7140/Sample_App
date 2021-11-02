2de758c84257c7bbe420b2fa7c3915c53d812c47
dcKim <kdc7140@naver.com>
2021-11-02 17:59:58 +0900

    calc sample is being developed

--- app/src/main/java/com/example/sample_app/Calculator.kt
+++ app/src/main/java/com/example/sample_app/Calculator.kt
@@ -13,8 +13,10 @@ class Calculator : AppCompatActivity() {
         setContentView(R.layout.activity_calculator)
     }
 
-    var calcNum : Int = 0;
-    var calcFlag : Int = 0;
+    var calcNum : Int = 0           //계산된 값
+    var calcFlag : Int = 0          //연산기호 클릭 flag
+    var rstFlag : Int = 0           // '='클릭 flag
+    var lastClickOp : Int = 0       //마지막누른 연산기호
 
     fun buttonClicked(v: View) {
         when (v.id) {
@@ -44,45 +46,84 @@ class Calculator : AppCompatActivity() {
     private fun btnNumClick(num: String){
         if(calcFlag == 0){
             resultView.append(num)
-            Log.d("KDCKDC1", "calcFlag : 0")
         }else {
-            resultView.setText("")
-            resultView.append(num)
             calcFlag = 0
-            Log.d("KDCKDC2", "calcFlag : 1")
+            if(rstFlag == 0){
+                resultView.setText("")
+                resultView.append(num)
+            }else{
+                resultView.setText("")
+                resultView2.setText("")
+                resultView.append(num)
+                rstFlag = 0
+            }
         }
     }
 
     //연산기호버튼
     private fun btnOperatorClicked(symbol: String){
 
-        var rv1 = Integer.parseInt(resultView.text.toString())
         calcFlag = 1
 
-        if(resultView2.text != ""){
+        var rv1 = Integer.parseInt(resultView.text.toString())
+
+        if(resultView2.text.toString() != ""){
             var rv2Text = resultView2.text.toString()
             var rv2Num = rv2Text.substring(0, rv2Text.length-1)
             calcNum = Integer.parseInt(rv2Num)
             Log.d("KDCKDC3", calcFlag.toString() )
 
-            resultView.setText((calcNum +rv1).toString())
+            when(symbol){
+                "+" -> resultView.setText((calcNum + rv1).toString())
+                "-" -> resultView.setText((calcNum - rv1).toString())
+                "X" -> resultView.setText((calcNum * rv1).toString())
+                "/" -> resultView.setText((calcNum / rv1).toString())
+            }
+
             resultView2.setText(resultView.text.toString() + symbol)
             calcNum = 0
         }else{
             resultView2.setText(resultView.text.toString() + symbol)
             resultView.setText("")
         }
+
+        if(symbol == "+"){
+
+        }
+
+
+
     }
 
     //초기화 버튼
     private fun viewReset(){
-        findViewById<TextView>(R.id.resultView).apply {
-            text = "";
-        }
+        resultView.setText("")
+        resultView2.setText("")
     }
+
 
     //계산
     private fun btnResult(){
+
+        var rv2Text = resultView2.text.toString()
+
+        if(rv2Text == ""){
+            resultView2.append(resultView.text.toString() + "=")
+        }else{
+            var rv2Num = rv2Text.substring(0, rv2Text.length-1)
+            calcNum = Integer.parseInt(rv2Num)
+
+            var rv1Num = Integer.parseInt(resultView.text.toString())
+
+            val rstNum = calcNum + rv1Num
+
+            resultView2.append(rv1Num.toString() + "=")
+
+            resultView.setText(rstNum.toString())
+        }
+
+        calcFlag = 1
+        rstFlag = 1
 
     }
 }
