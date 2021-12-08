e7fb1fb9ae085c20564ed921f7222f1e0fcf41e3
dcKim <kdc7140@naver.com>
2021-12-08 17:59:49 +0900

    pagemove sample is being develope

--- app/src/main/java/com/example/sample_app/PageMove.kt
+++ app/src/main/java/com/example/sample_app/PageMove.kt
@@ -1,16 +1,29 @@
 package com.example.sample_app
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 
 class PageMove : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_move)
 
+        var getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                val myData: Intent? = result.data
+//                val stringData = myData.getStringExtra("dataName")
+                //또는 이렇게
+                val stringData2 = result.data?.getStringExtra("param")
+                // 기타 등등 작업 수행
+                Toast.makeText(this ,"answer : " + stringData2, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         var btnPageMove : Button = findViewById(R.id.btnPageMove)
         btnPageMove.setOnClickListener{
@@ -22,10 +35,17 @@ class PageMove : AppCompatActivity() {
             val intent = Intent(this, PageMove2::class.java).apply {
                 putExtra(EXTRA_MESSAGE, message)
             }
-            startActivityForResult(intent, 200)
+            getResult.launch(intent)
+//            startActivity(intent)
 
+//            val intent = Intent(this, PageMove2::class.java)
+//            getResult.launch(intent)
         }
 
+
+
     }
+
+
 
 }
--- app/src/main/java/com/example/sample_app/PageMove2.kt
+++ app/src/main/java/com/example/sample_app/PageMove2.kt
@@ -1,8 +1,13 @@
 package com.example.sample_app
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_page_move2.*
 
 class PageMove2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +16,21 @@ class PageMove2 : AppCompatActivity() {
 
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-
+        revView.setText(message)
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.revView).apply {
-            text = message
+//        val textView = findViewById<TextView>(R.id.revView).apply {
+//            text = message
+//        }
+
+
+        var btnSend : Button = findViewById(R.id.sendRev)
+        btnSend.setOnClickListener {
+            var rtText = revView.text.toString()
+
+            var intent = Intent(this, PageMove::class.java)
+            intent.putExtra("param", rtText)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
         }
     }
 }
