738bcb78f5335fc0615fbb8da5c430badd623002
dcKim <kdc7140@naver.com>
2022-01-05 17:51:31 +0900

    event sample is being develope

--- app/src/main/java/com/example/sample_app/KotlinEvent.kt
+++ app/src/main/java/com/example/sample_app/KotlinEvent.kt
@@ -5,12 +5,22 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sample_app.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_kotlin_event.*
 
 class KotlinEvent : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    private val binding get() = mBinding!!
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin_event)
+//        setContentView(R.layout.activity_kotlin_event)
+
+        setContentView(binding.root)
 
         val view = findViewById<View>(R.id.view1)
         view.setOnTouchListener { view, motionEvent ->
@@ -19,10 +29,13 @@ class KotlinEvent : AppCompatActivity() {
             val y = motionEvent.y
             if (state == MotionEvent.ACTION_DOWN) {
                 println("손가락 눌림 : $x, $y")
+                textView1.setText("손가락 눌림 : $x, $y")
             } else if (state == MotionEvent.ACTION_MOVE) {
                 println("손가락 움직임 : $x, $y")
+                textView2.setText("손가락 움직임 : $x, $y")
             } else if (state == MotionEvent.ACTION_UP) {
                 println("손가락 땜 : $x, $y")
+                textView3.setText("손가락 땜 : $x, $y")
             }
             true
         }
